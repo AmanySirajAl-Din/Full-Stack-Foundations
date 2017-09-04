3000a4968665af2e78fa7b1353faf26cffa8efdd
Amany Siraj <amanysirajaldin@gmail.com>
2017-09-04 15:46:05 +0200

    Add comments to explain in part 01 & 02

--- Lesson-3/01_First-Flask-App/project.py
+++ Lesson-3/01_First-Flask-App/project.py
@@ -1,12 +1,29 @@
+# this the minimum flask app
+# first, import Flask class
 from flask import Flask
+# next, create an instance of Flask class
+# with the name of the running app __name__
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')  # the decorator
 @app.route('/hello')
 def HelloWorld():
     return "Hello World"
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
+# makes sure the server only runs if the script is
+# excuted directly from the Python interpreter
+# and not used as imported moudule
     app.debug = True
+    # Flask take care of,
+    # restarting our server each time
+    # we made a modification to our code
+    # with app.debug = True,
+    # the server will reload itself each time
+    # the code changes
+    # and provide a helpful debug in the browser
     app.run(host='0.0.0.0', port=5000)
+    # to run the local server with our app
+    # '0.0.0.0' this tells the web server to
+    # listen to all public IP addresses
--- Lesson-3/02_Adding-Database-to-Flask-Application/project.py
+++ Lesson-3/02_Adding-Database-to-Flask-Application/project.py
@@ -1,10 +1,12 @@
 from flask import Flask
+# bring our CRUD functionality into my app
+# 1- import the code for SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
-
+# 2- And the DB engine in sissionmaker like lesson 1&2
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
@@ -15,13 +17,19 @@ session = DBSession()
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
+    # 3- change my HelloWorld fun
     restaurant = session.query(Restaurant).first()
+    # query to grap the 1st restaurant
+    # out of my DB
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    # list out all of the menu items
     output = ''
     for i in items:
         output += i.name
-        output += '</br>'
-    return output
+        output += '</br>'  
+        # add it to make my output easier to read
+    return output  # return it so that my user sees it from the browser
+    # and stored in a string called output
 
 if __name__ == '__main__':
     app.debug = True
