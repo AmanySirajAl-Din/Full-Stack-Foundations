bf69b6197eeb5ba26d789d24caca59c8c8ab4464
Amany Siraj <amanysirajaldin@gmail.com>
2017-09-04 16:25:02 +0200

    Add comments to explain in part 04

--- Lesson-3/04_Routing/project.py
+++ Lesson-3/04_Routing/project.py
@@ -11,9 +11,22 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# To add var to URL,
+# specify a rule with <type:var_name>
+# type ==> can be (int, string or path)
 @app.route('/restaurants/<int:restaurant_id>/')
+# make a new route for my menue app
+# by using the restaurant_id to specify 
+# which menu I want to see
+# in /<int:restaurant_id>/')
+# the slash in the end (the trailing slash)
+# Flask will render the page even 
+# when it's not there in the URL
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    # filter_by to filter results to restaurant_id
+    # .one() requires that there is only one result in the result set;
+    # it is an error if the database returns 0 or 2 or more results and an exception will be raised.
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for i in items:
